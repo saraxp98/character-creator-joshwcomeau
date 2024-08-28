f2554511e9aa173676920fba106440109166a8c5
Sara Barichello <sarab@ericed.it>
2024-08-28 09:56:17 +0200

    Exercises 2 done!

--- src/components/MaxWidthWrapper/MaxWidthWrapper.js
+++ src/components/MaxWidthWrapper/MaxWidthWrapper.js
@@ -3,18 +3,15 @@ import React from 'react';
 import styles from './MaxWidthWrapper.module.css';
 
 const MaxWidthWrapper = ({
-  children,
-  className = '',
-  ...delegated
-}) => {
-  return (
-    <div
-      className={`${styles.maxWidthWrapper} ${className}`}
-      {...delegated}
-    >
-      {children}
-    </div>
-  );
+                             children,
+                             className = '',
+                             ...delegated
+                         }) => {
+    return (
+        <div className={`${styles.maxWidthWrapper} ${className}`} {...delegated}>
+            {children}
+        </div>
+    );
 };
 
 export default MaxWidthWrapper;
